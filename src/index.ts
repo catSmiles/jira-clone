0f78081560f2038218c830096b037536c21b99a3
levantrung81 <levantrung.fe2101@gmail.com>
2022-12-06 03:29:39 +0700

    Play with middleware

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import 'module-alias/register';
 
-import createDatabaseConnection from 'database/createConnection';
+// import createDatabaseConnection from 'database/createConnection';
 
 // establish database connection
-const establishDatabaseConnection = async (): Promise<void> => {
-  try {
-    await createDatabaseConnection();
-    console.log('db connect successful');
-  } catch (e) {
-    console.log(e);
-  }
-};
+// const establishDatabaseConnection = async (): Promise<void> => {
+//   try {
+//     await createDatabaseConnection();
+//     console.log('db connect successful');
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
 // initialize express
 const initializeExpress = (): void => {
@@ -19,12 +19,27 @@ const initializeExpress = (): void => {
   const app = express();
   app.use(express.json());
 
+  app.get(
+    '/middleware',
+    (req, res, next) => {
+      if (['veVip', 'veThuong'].includes(req.query.ve)) {
+        next();
+      }
+      res.status(403).json({ message: 'Access denied' });
+    },
+    (_req, res) => {
+      res.json({ message: 'Successfully' });
+    },
+  );
+
   // start express server
-  app.listen(3000);
+  app.listen(3000, () => {
+    console.log('server running on port 3000');
+  });
 };
 
 const initializeApp = async (): Promise<void> => {
-  await establishDatabaseConnection();
+  // await establishDatabaseConnection();
   initializeExpress();
 };
 
