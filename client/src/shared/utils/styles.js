49e0c73a2a66d36b746552ac34b4dab87b5d712a
levantrung81 <levantrung.fe2101@gmail.com>
2022-12-27 09:20:20 +0700

    Setup fonts, create BaseStyles and normalizeStyles

---
+++ client/src/shared/utils/styles.js
@@ -0,0 +1,49 @@
+import { css } from 'styled-components'; // A helper function to generate CSS from a template literal with interpolations
+
+export const color = {
+  textDarkest: '#172b4d',
+  textDark: '#42526E',
+  textMedium: '#5E6C84',
+  textLight: '#8993a4',
+  textLink: '#0052cc',
+};
+
+export const font = {
+  regular: 'font-family: "CircularStdBook"; font-weight: normal;',
+  medium: 'font-family: "CircularStdMedium"; font-weight: normal;',
+  bold: 'font-family: "CircularStdBold"; font-weight: normal;',
+  black: 'font-family: "CircularStdBlack"; font-weight: normal;',
+  size: size => `font-size: ${size}px;`,
+};
+
+export const mixin = {
+  placeholderColor: colorValue => css`
+    ::-webkit-input-placeholder {
+      color: ${colorValue} !important;
+      opacity: 1 !important;
+    }
+    :-moz-placeholder {
+      color: ${colorValue} !important;
+      opacity: 1 !important;
+    }
+    ::-moz-placeholder {
+      color: ${colorValue} !important;
+      opacity: 1 !important;
+    }
+    :-ms-input-placeholder {
+      color: ${colorValue} !important;
+      opacity: 1 !important;
+    }
+  `,
+  link: (colorValue = color.textLink) => css`
+    cursor: pointer;
+    color: ${colorValue};
+    ${font.medium}
+    &:hover, &:visited, &:active {
+      color: ${colorValue};
+    }
+    &:hover {
+      text-decoration: underline;
+    }
+  `,
+};
